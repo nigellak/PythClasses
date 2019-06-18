ca3df768cb7e77d802cbe5cadc6ae188bc53e28c
nigellak <nigellyo357@gmail.com>
2019-06-18 14:26:09 +0300

    Python Classes

---
+++ class1.py
@@ -0,0 +1,22 @@
+
+# classes,objects,instances
+
+class Nigella:
+    pass
+
+table1 = Nigella()
+table1.comp = 'hp'
+table1.mouse = 'dell'
+table1.envelope = 'khaki'
+
+table2 = Nigella()
+table2.comp = 'dell'
+table2.phone = 'nokia'
+table2.book = 'counter book'
+
+table3 = Nigella()
+table3.bottle = 'soda'
+table3.phone = 'tecno'
+table3.book = 'exercise book'
+
+print(table2.comp)
