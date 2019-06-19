ff28fdca4a9e079dab1dd777613a39fe08a24443
nigellak <nigellyo357@gmail.com>
2019-06-19 13:55:10 +0300

    .

---
+++ tuples.py
@@ -0,0 +1,11 @@
+names = ('Nigella', 'king', 'mouse', 'eMobilis')
+print(names)
+print(names[0])
+print(names[1:])
+print(names[1:3])
+# will only go from 1 to 2
+print(names[1:3+1])
+print(names*2)
+
+# accepts name and salary from input of user, details of employees
+# Finally display the total count of employees
